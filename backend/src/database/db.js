5ea31961e1c5c21e1dd0273454bccd926b1a2f15
Tobias <tobiasvega1210@gmail.com>
2024-08-15 16:39:02 -0300

    se conecto a mongoDB

--- backend/src/database/db.js
+++ backend/src/database/db.js
@@ -8,3 +8,18 @@ export const newConnection = async () => {
         database: "sosmissions"
     })
 }
+
+import  mongoose  from 'mongoose';
+
+export const connectDB = async () => {
+    try {
+        await mongoose.connect('mongodb://localhost:27017/sosmissions', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        });
+        console.log('Conectado a MongoDB');
+    } catch (error) {
+        console.error('Error al conectar a MongoDB:', error.message);
+        process.exit(1);
+    }
+}
--- backend/src/routes/post.routes.js
+++ backend/src/routes/post.routes.js
@@ -1,13 +1,6 @@
 import { Router } from 'express';
 export const router = Router();
 
-import {
-    getAllPost,
-    getPostById,
-    createPost,
-    updatePost,
-    deletePost
-} from '../controllers/post.controllers.js';
 
 postRouter.get('/post', getAllPost);
 postRouter.get('/post', getPostById);
