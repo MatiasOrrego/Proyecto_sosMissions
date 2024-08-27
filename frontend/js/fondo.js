e1561ad007957901255b57cd9c8cd6a4d1eaf708
Quiroga J <juanqi3489@gmail.com>
2024-08-27 14:11:08 -0300

    Se pudo agregar titulo y se estilizo la barra de navegación

--- frontend/js/fondo.js
+++ frontend/js/fondo.js
@@ -1,7 +1,7 @@
 let c = document.getElementById('canvas');
 let $ = c.getContext('2d');
 
-// Convertir colores hex a RGB
+// Función para aplicar el color a un pixel
 let col = function(x, y, r, g, b) {
     $.fillStyle = `rgb(${r},${g},${b})`;
     $.fillRect(x, y, 1, 1);
@@ -9,23 +9,26 @@ let col = function(x, y, r, g, b) {
 
 // Colores base en RGB
 const colors = {
-    color1: { r: 3, g: 12, b: 21 },  // #030c15
+    color1: { r: 3, g: 12, b: 21 },   // #030c15
     color2: { r: 116, g: 6, b: 33 },  // #740621
-    color3: { r: 228, g: 1, b: 46 }   // #e4012e
+    color3: { r: 228, g: 1, b: 46 },  // #e4012e
+    color4: { r: 255, g: 0, b: 47 }   // #ff002f
 };
 
 // Funciones para calcular colores RGB con base en el tiempo
 let R = function(x, y, t) {
-    // Usar colores base para generar una variación
-    return Math.floor(colors.color1.r + (colors.color2.r - colors.color1.r) * (Math.sin(t + x / 10) + 1) / 2);
+    return Math.floor(colors.color1.r + (colors.color2.r - colors.color1.r) * (Math.sin(t + x / 10) + 1) / 2
+        + (colors.color3.r - colors.color2.r) * (Math.cos(t + y / 20) + 1) / 2);
 };
 
 let G = function(x, y, t) {
-    return Math.floor(colors.color1.g + (colors.color3.g - colors.color1.g) * (Math.sin(t + y / 10) + 1) / 2);
+    return Math.floor(colors.color1.g + (colors.color3.g - colors.color1.g) * (Math.sin(t + y / 10) + 1) / 2
+        + (colors.color4.g - colors.color3.g) * (Math.cos(t + x / 20) + 1) / 2);
 };
 
 let B = function(x, y, t) {
-    return Math.floor(colors.color1.b + (colors.color3.b - colors.color2.b) * (Math.sin(t + (x + y) / 20) + 1) / 2);
+    return Math.floor(colors.color1.b + (colors.color3.b - colors.color2.b) * (Math.sin(t + (x + y) / 20) + 1) / 2
+        + (colors.color4.b - colors.color3.b) * (Math.cos(t + (x - y) / 20) + 1) / 2);
 };
 
 let t = 0;
@@ -36,7 +39,7 @@ let run = function() {
             col(x, y, R(x, y, t), G(x, y, t), B(x, y, t));
         }
     }
-    t = t + 0.050; // Reducir la velocidad
+    t = t + 0.025; // Reducir la velocidad
     window.requestAnimationFrame(run);
 };
 
