827dd00c54a9a6cb3fd35114998112754024e597
Tobias <tobiasvega1210@gmail.com>
2024-10-13 23:05:12 -0300

    Se agregaron los roles para los usuarios

--- backend/src/models/post.model.js
+++ backend/src/models/post.model.js
@@ -1,22 +1,22 @@
 import { conn } from "../database/db.js";
 
-export const createPost = async (title, description, userId, fecha_publi) => {
-const [result] = await conn.query(`INSERT INTO post (title, description, userId, fecha_publi) VALUES (?,?,?,CURRENT_DATE())`, [title, description, userId, fecha_publi])
+export const createPost = async (title, description, medicId, fecha_publi, categoryId) => {
+const [result] = await conn.query(`INSERT INTO post (title, description, medicId, fecha_publi, categoryId) VALUES (?,?,?,CURRENT_DATE())`, [title, description, medicId, fecha_publi, categoryId])
 
-return { id: result.insertId, title, description, userId, fecha_publi }
+return { id: result.insertId, title, description, medicId, fecha_publi }
 };
 
-export const getPosts = async (userId) => {
-  const [result] = await conn.query(`SELECT * FROM post WHERE userId = ?`, [userId]);
+export const getPosts = async (medicId) => {
+  const [result] = await conn.query(`SELECT * FROM post WHERE medicId = ?`, [medicId]);
   return result
 };
 
-export const getPostById = async (id, userId) => {
-  const [result] = await conn.query(`SELECT * FROM post WHERE id = ? AND userId = ?`, [id, userId])
+export const getPostById = async (id, medicId) => {
+  const [result] = await conn.query(`SELECT * FROM post WHERE id = ? AND medicId = ?`, [id, medicId])
   return result[0] || null;
 };
 
-export const deletePostById = async (id, userId) => {
-  const [result] = await conn.query(`DELETE FROM post WHERE id = ? AND userId = ?`, [id, userId]);
+export const deletePostById = async (id, medicId) => {
+  const [result] = await conn.query(`DELETE FROM post WHERE id = ? AND medicId = ?`, [id, medicId]);
   return result.affectedRows > 0
 };
