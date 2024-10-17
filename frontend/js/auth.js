b31af3793287cbb2eb055480dda513356051f76d
Tobias <tobiasvega1210@gmail.com>
2024-10-17 16:00:25 -0300

    Se agregaron calificación de estrellas

--- frontend/js/auth.js
+++ frontend/js/auth.js
@@ -1,5 +1,9 @@
 document.addEventListener('DOMContentLoaded', async () => {
   const btnLoginRegister = document.getElementById('btn-lr');
+  const addPostBtn = document.getElementById('addPostBtn'); // Botón de añadir publicación
+
+  // Inicialmente ocultar el botón de añadir publicación
+  addPostBtn.style.display = 'none';
 
   try {
     const response = await fetch('http://localhost:3000/auth/me', {
@@ -14,6 +18,14 @@ document.addEventListener('DOMContentLoaded', async () => {
     const data = await response.json();
 
     if (data) {
+      // Si el usuario tiene roleId 2 (medic), mostrar el botón de añadir publicación
+      if (data.roleId === 2) {
+        addPostBtn.style.display = 'block'; // Mostrar el botón
+      } else {
+        addPostBtn.style.display = 'none'; // Ocultar el botón si no es roleId 2
+      }
+
+      // Mostrar el botón de cerrar sesión si el usuario está autenticado
       btnLoginRegister.innerHTML = `<button class="btn" id="logout">Cerrar Sesión</button>`;
 
       // Agrega el evento para cerrar sesión
@@ -32,12 +44,17 @@ document.addEventListener('DOMContentLoaded', async () => {
         }
       });
     } else {
+      // Si no está autenticado, mostrar los botones de iniciar sesión y registro
       btnLoginRegister.innerHTML = `
         <button class="btn" id="loginBtn"><a href="/frontend/sesion.html">Iniciar Sesión</a></button>
         <button class="btn" id="registerBtn"><a href="/frontend/registro.html">Registrarse</a></button>
       `;
+      addPostBtn.style.display = 'none'; // Asegurarse de que el botón esté oculto si no está autenticado
     }
   } catch (error) {
+    // Si hay un error o el usuario no está autenticado, ocultar el botón de añadir publicación
+    addPostBtn.style.display = 'none';
+
     // Manejar errores de autenticación
     btnLoginRegister.innerHTML = `
       <button class="btn" id="loginBtn"><a href="/frontend/sesion.html">Iniciar Sesión</a></button>
